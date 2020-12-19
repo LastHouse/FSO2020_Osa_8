676ee6a92362b4b0b9033deafae3bbc09cfe71d3
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-12-19 18:31:39 +0200

    Recommendations.js missing from last push

---
+++ Osa_8_library_frontend/src/components/Recommendations.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS_SEARCH } from '../queries';
+
+const Recommendations = ({ show, favoriteGenre, setError }) => {
+  const genre = favoriteGenre;
+
+  const { loading, error, data } = useQuery(ALL_BOOKS_SEARCH, {
+    variables: { genre },
+  });
+
+  if (error) {
+    setError(error.graphQLErrors[0].message);
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
+  if (!show | !favoriteGenre) {
+    return null;
+  }
+
+  if (loading) return <p>Loading ...</p>;
+
+  return (
+    <div>
+      <h2>Books in your favorite genre "{favoriteGenre}"</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {data.allBooks.map((b) => (
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default Recommendations;
